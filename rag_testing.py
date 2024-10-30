9a494551bf5d185c7024eacc648a8f741c0bbde4
Xiaocong Yang <79353358+Xiaocong-Yang@users.noreply.github.com>
2024-10-29 20:02:48 -0500

    Add files via upload

--- rag_testing.py
+++ rag_testing.py
@@ -18,7 +18,7 @@ from rank_bm25 import BM25Okapi
 class ModifiedModel(nn.Module):
     def __init__(self):
         super(ModifiedModel, self).__init__()
-        self.bert = AutoModel.from_pretrained('BAAI/bge-large-zh-v1.5', torch_dtype=torch.bfloat16)
+        self.bert = AutoModel.from_pretrained('BAAI/bge-large-en-v1.5', torch_dtype=torch.bfloat16)
         self.extra_pooler = torch.nn.Sequential(torch.nn.Linear(1024, 1024, dtype=torch.bfloat16), torch.nn.ReLU(), torch.nn.Linear(1024, 1024, dtype=torch.bfloat16))
 
     
@@ -44,62 +44,77 @@ class Retriever():
     
 class DenseRetriever(Retriever):
     def __init__(self, retriever_trained=True, model_modified=True):
-        self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-large-zh-v1.5')
+        self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-large-en-v1.5')
         self.retriever_trained = retriever_trained
         if retriever_trained:
             # self.model = AutoModel.from_pretrained("/shared/xiaocong/graph_rag/bge-large-zh-v1.5_math23k_10epoch_v5_masked.pt").cuda()
-            self.model = torch.load("/shared/xiaocong/graph_rag/bge-large-zh-v1.5_math23k_6epoch_v6_masked.pt")
+            self.model = torch.load("/shared/xiaocong/graph_rag/bge-large-en-v1.5_math23k_10epoch_v6_masked.pt", map_location="cpu")
             # print(self.model)
             # self.model = AutoModel.from_pretrained('BAAI/bge-large-zh-v1.5').cuda()
             # # load checkpoint
             # self.model.load_state_dict(torch.load("/shared/xiaocong/graph_rag/bge-large-zh-v1.5_math23k_10epoch_v5_masked.pt"))
         else:
-            self.model = AutoModel.from_pretrained('BAAI/bge-large-zh-v1.5')
+            self.model = AutoModel.from_pretrained('BAAI/bge-large-en-v1.5')
         self.model_modified = model_modified
-        self.corpus_embeddings = torch.load("tal_corpus_embeddings_trained_v1_Chinese.pt").to("cuda:0")
-        self.corpus = load_dataset("json",data_files="/shared/xiaocong/TAL-SCQ5K/ch_single_choice_constructed_5K/ch_single_choice_train_3K.jsonl", split="train")
-        # self.corpus_embeddings, self.corpus = self._encode_corpus("tal")
+        # self.corpus_embeddings = torch.load("tal_corpus_embeddings_trained_v1_Chinese.pt").to("cuda:0")
+        # self.corpus = load_dataset("json",data_files="/shared/xiaocong/TAL-SCQ5K/ch_single_choice_constructed_5K/ch_single_choice_train_3K.jsonl", split="train")
+        self.corpus_embeddings, self.corpus = self._encode_corpus("openai/gsm8k")
+        # if self.retriever_trained:
+        #     torch.save(self.corpus_embeddings, "metamathqa_corpus_embeddings_trained_v1.pt")
+        #     with open("metamathqa_corpus_trained_v1.json", "w") as f:
+        #         f.write(json.dumps(self.corpus, ensure_ascii=False))
+        # else:
+        #     torch.save(self.corpus_embeddings, "metamathqa_corpus_embeddings_untrained_v1.pt")
+        #     with open("metamathqa_corpus_untrained_v1.json", "w") as f:
+        #         f.write(json.dumps(self.corpus, ensure_ascii=False))
+        # self.corpus_embeddings = torch.load("metamathqa_corpus_embeddings_trained_v1.pt").to("cuda:0")
+        # with open("metamathqa_corpus_trained_v1.json", "r") as f:
+        #     self.corpus = json.load(f)
         # print("Corpus embeddings shape: ", self.corpus_embeddings.shape)
         # print("Corpus length: ", len(self.corpus))
         # assert len(self.corpus_embeddings) == len(self.corpus) == 3000
         # # # # save corpus embeddings
-        # torch.save(self.corpus_embeddings, "tal_corpus_embeddings_trained_v1_Chinese.pt")
+        # torch.save(self.corpus_embeddings, "gaokao_math_corpus_embeddings_untrained_v1_Chinese.pt")
         # # # save corpus
         # with open("ape210k_corpus_trained_v1_Chinese.json", "w") as f:
         #     f.write(json.dumps(self.corpus, ensure_ascii=False))
         # if not self.retriever_trained:
-        #     self.corpus_embeddings = torch.load("ape210k_corpus_embeddings_untrained_v1_Chinese.pt").to("cuda:0")
-        #     with open("ape210k_corpus_untrained_v1_Chinese.json", "r") as f:
-        #         self.corpus = json.load(f)
+        #     self.corpus_embeddings = torch.load("/shared/xiaocong/graph_rag/math23k_corpus_embeddings_untrained_Chinese.pt", map_location="cpu").to("cuda:0")
+        #     self.corpus = load_dataset("Gxg/Math23K", split="train")
         # else:
-        #     self.corpus_embeddings = torch.load("ape210k_corpus_embeddings_trained_v1_Chinese.pt").to("cuda:4")
-        #     with open("ape210k_corpus_trained_v1_Chinese.json", "r") as f:
-        #         self.corpus = json.load(f)
+        #     self.corpus_embeddings = torch.load("/shared/xiaocong/graph_rag/math23k_corpus_embeddings_trained_v6.2_Chinese.pt").to("cuda:2")
+        #     self.corpus = load_dataset("Gxg/Math23K", split="train")
         # self.corpus_embeddings = torch.load("gsm8k_corpus_embeddings_trained.pt")
         # self.corpus_embeddings = torch.load("gsm8k_corpus_embeddings_untrained.pt")
         # self.corpus_embeddings = torch.load("math23k_corpus_embeddings_trained_v6_Chinese.pt")
         
-    def _average_pool(self, last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
-        last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
-        return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
+    # def _average_pool(self, last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
+    #     last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
+    #     return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
     def _encode_corpus(self, corpus):
         # accelerator = Accelerator()
-        if corpus == "tal":
+        if corpus == "gaokao_math":
             # corpus = load_dataset("json",data_files="/shared/xiaocong/ape210k/data/train.ape.jsonl", split="train")
-            corpus = load_dataset("json",data_files="/shared/xiaocong/TAL-SCQ5K/ch_single_choice_constructed_5K/ch_single_choice_train_3K.jsonl", split="train")
+            corpusA = load_dataset("json",data_files="/shared/xiaocong/GAOKAO-Bench/Data/Objective_Questions/2010-2022_Math_I_MCQs.json", split="train")["example"]
+            corpusB = load_dataset("json",data_files="/shared/xiaocong/GAOKAO-Bench/Data/Objective_Questions/2010-2022_Math_II_MCQs.json", split="train")["example"]
+            # combine two datasets
+            corpus = corpusA + corpusB
         else:
-            corpus = load_dataset(corpus, split="train")
+            corpus = load_dataset(corpus, "main", split="train")
         all_embeddings = []
-        # all_data = {"original_text": [], "equation": [], "answer": []}
-        dataloader = torch.utils.data.DataLoader(corpus, batch_size=64, shuffle=False, collate_fn=collate_fn)
+        all_data = {"question": [], "answer": []}
+        dataloader = torch.utils.data.DataLoader(corpus, batch_size=16, shuffle=False)
         # _model, dataloader = accelerator.prepare(self.model, dataloader)
         _model = self.model.to("cuda:0")
         with torch.no_grad():
             for batch in tqdm(dataloader):
                 questions = batch["question"]
                 inputs = self.tokenizer(questions, return_tensors="pt", padding=True, truncation=True, max_length=512)
-                inputs = {k: v.to(_model.bert.device) for k, v in inputs.items()}
+                if self.model_modified:
+                    inputs = {k: v.to(_model.bert.device) for k, v in inputs.items()}
+                else:
+                    inputs = {k: v.to(_model.device) for k, v in inputs.items()}
                 if self.model_modified:
                     embeddings = _model(inputs)
                 else:
@@ -112,8 +127,11 @@ class DenseRetriever(Retriever):
                 # all_data["original_text"].extend(accelerate.utils.gather_object(batch["original_text"]))
                 # all_data["equation"].extend(accelerate.utils.gather_object(batch["equation"]))
                 # all_data["answer"].extend(accelerate.utils.gather_object(batch["answer"]))
+                all_data["question"].extend(batch["question"])
+                all_data["answer"].extend(batch["answer"])
         all_embeddings = torch.cat(all_embeddings)
-        return all_embeddings, corpus
+        assert len(all_embeddings) == len(all_data["question"])
+        return all_embeddings, all_data
     
     def encode_query(self, query):
         self.model = self.model.to("cuda:0")
@@ -165,10 +183,10 @@ class SymbolicRetriever(Retriever):
         topk_indices = torch.topk(similarity_matrix, topk).indices
         return topk_indices
 
-def collate_fn(batch):
-    questions = [item["problem"] + "\nOptions: " + str(item["answer_option_list"]) for item in batch]
-    answers = [item["answer_analysis"][0] + "#### Answer: " + str(item["answer_value"]) for item in batch]
-    return {"question": questions, "answer": answers}
+# def collate_fn(batch):
+#     questions = [item["problem"] + "\nOptions: " + str(item["answer_option_list"]) for item in batch]
+#     answers = [item["answer_analysis"][0] + "#### Answer: " + str(item["answer_value"]) for item in batch]
+#     return {"question": questions, "answer": answers}
 
 # def collate_fn(batch):
 #     original_questions = [item['original_text'] for item in batch]
@@ -182,40 +200,45 @@ def collate_fn(batch):
 #     answers = [item['ans'] for item in batch]
 #     return {"original_text": original_questions, "equation": equations, "answer": answers}
 
+
+
 if __name__ == "__main__":
-    retriever = DenseRetriever(retriever_trained=True, model_modified=True)
+    # retriever = DenseRetriever(retriever_trained=False, model_modified=False)
     # generator = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-3.2-1B-Instruct', torch_dtype=torch.bfloat16).to("cuda:3")
-    pipeline = pipeline("text-generation", model="Qwen/Qwen2.5-1.5B-Instruct",torch_dtype=torch.bfloat16,device="cuda:0")
+    pipeline = pipeline("text-generation", model="meta-llama/Llama-3.2-1B-Instruct",torch_dtype=torch.bfloat16,device="cuda:0")
     
     # tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-3.2-1B-Instruct')
     # tokenizer.pad_token = tokenizer.eos_token
     # train_dataset = load_dataset("Gxg/Math23K", split="train")
-    # train_dataset = load_dataset("json",data_files="/shared/xiaocong/ape210k/data/train.ape.jsonl", split="train")
+    # train_dataset = load_dataset("json",data_files="/shared/xiaocong/TAL-SCQ5K/ch_single_choice_constructed_5K/ch_single_choice_train_3K.jsonl", split="train")
     # val_dataset = load_dataset("json",data_files="/shared/xiaocong/ape210k/data/valid.ape.jsonl", split="train")
-    val_dataset = load_dataset("json",data_files="/shared/xiaocong/TAL-SCQ5K/ch_single_choice_constructed_5K/ch_single_choice_test_2K.jsonl", split="train")
+    train_dataset = load_dataset("openai/gsm8k","main", split="train")
+    val_dataset = load_dataset("openai/gsm8k","main", split="test")
     # retriever = SymbolicRetriever(train_dataset)
-    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False, collate_fn=collate_fn)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=16, shuffle=False)
     
-    with open("tal_val_results_trained_8_shots.jsonl", "w") as f:
-        for batch in tqdm(val_dataloader):
+    # with open("gsm8k_val_results_trained_8_shots_qwen2.5.jsonl" if retriever.retriever_trained else "gsm8k_val_results_untrained_8_shots_qwen2.5.jsonl", "w") as f:
+    
+    for batch in tqdm(val_dataloader):
+        with open("gsm8k_test_results_randomly_8_shots_llama3.2_em_maj_20_samples.jsonl", "a+") as f:
             prompts = []
             # questions = batch["original_text"]
-            retrieved_indices = retriever.retrieve(batch["question"])
-            for i in range(retrieved_indices.shape[0]):
+            # retrieved_indices = retriever.retrieve(batch["question"])
+            for i in range(len(batch["question"])):
                 prompt = [{"role":"system", "content":"Answer the question following the format of given examples."}]
-                for j in range(retrieved_indices.shape[1]):
-                    prompt.append({"role": "user", "content": retriever.corpus["problem"][retrieved_indices[i, j]] + "\n选项: " + str(retriever.corpus["answer_option_list"][retrieved_indices[i, j]])})
-                    prompt.append({"role": "assistant", "content": retriever.corpus["answer_analysis"][retrieved_indices[i, j]][0] + "#### 答案: " + str(retriever.corpus["answer_value"][retrieved_indices[i, j]])})
+                # for j in range(retrieved_indices.shape[1]):
+                #     prompt.append({"role": "user", "content": retriever.corpus["question"][retrieved_indices[i, j]]})
+                #     prompt.append({"role": "assistant", "content": retriever.corpus["answer"][retrieved_indices[i, j]]})
                 # random sample 8 examples from the training dataset
-                # for _ in range(8):
-                #     random_index = torch.randint(0, len(train_dataset), (1,)).item()
-                #     prompt.append({"role": "user", "content": train_dataset["original_text"][random_index]})
-                #     prompt.append({"role": "assistant", "content": train_dataset["equation"][random_index]})
+                for _ in range(8):
+                    random_index = torch.randint(0, len(train_dataset), (1,)).item()
+                    prompt.append({"role": "user", "content": train_dataset["question"][random_index]})
+                    prompt.append({"role": "assistant", "content": train_dataset["answer"][random_index]})
                 prompt.append({"role": "user", "content": batch["question"][i]})
                 prompts.append(prompt)
-            outputs = pipeline(prompts, max_new_tokens=512)
+            outputs = pipeline(prompts, max_new_tokens=512, num_return_sequences=10)
             for i, output in enumerate(outputs):
-                line = {"answer": batch["answer"][i], "generated": output[0]["generated_text"][-1]["content"]}
+                line = {"answer": batch["answer"][i], "generated": [output[j]["generated_text"][-1]["content"] for j in range(len(output))]}
                 f.write(json.dumps(line, ensure_ascii=False) + "\n")
     # with open("gsm8k_test_results_zero_shot.jsonl", "w") as f:
     #     with torch.no_grad():
